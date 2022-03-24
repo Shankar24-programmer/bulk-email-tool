ded66c26443416410715a2126bff4044efffa15f
Muthu Sankar <reachtoshankarsk@gmail.com>
2022-03-24 18:17:26 +0530

    bulk email tool

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import cors from 'cors'
+import dotenv from "dotenv";
+
+dotenv.config();
+const app=express()
+app.use(cors());
+app.use(express.json())
+var sender = nodemailer.createTransport({
+    service: "gmail",   
+    auth: {   
+    user: process.env.email,   
+    pass: process.env.password,   
+    },    
+    });
+    
+
+app.post("/sendprocess",async function(req, res){   
+const {to,subject,content}=req.body;   
+    var composemail = {  
+    from: process.env.email,  
+    to: to,
+   subject: subject,
+    text: content,  
+    };
+    
+    sender.sendMail(composemail, function (error, info) { 
+    if (error) {
+    console.log(error); 
+    res.status(404).send("server busy");
+    } else {  
+    console.log("success " + info.response);  
+    res.send("sucess");
+    }   
+    });
+
+
+})
+
+app.listen(4000, () => {
+
+console.log(`Example app listening on port `)
+
+})
